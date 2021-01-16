b02f9c7929a773b7d92c16be36261f87f61225da
Jiusen <2434732570@qq.com>
2021-01-17 03:17:47 +0800

    怎么禁止自我保护

--- cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -66,12 +66,12 @@ public class PaymentController
     public Object discovery() {
         List<String> services = discoveryClient.getServices();
         for (String element : services) {
-            log.info("****elemnet" + element);
+            log.info("****elemnet: " + element);
         }
 
         List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
         for (ServiceInstance instance : instances) {
-            log.info(instance.getServiceId() + "\t" + instance + "\t" + instance.getPort() + "\t" + instance.getUri());
+            log.info(instance.getServiceId() + "\t" + instance.getHost() + "\t" + instance.getPort() + "\t" + instance.getUri());
         }
         return this.discoveryClient;
     }
