e5ca44293cdfc3f4552437f76718316fc35b4e8e
Jiusen <2434732570@qq.com>
2021-01-20 01:00:16 +0800

    OpenFeign服务调用

---
+++ cloud-consumer-fegin-order80/src/main/java/com/atguigu/springcloud/OrderFeignMain80.java
@@ -0,0 +1,19 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * @author Jiusen Guo
+ * @date 2021/1/20 0:03
+ */
+@SpringBootApplication
+@EnableFeignClients
+public class OrderFeignMain80
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(OrderFeignMain80.class, args);
+    }
+}
