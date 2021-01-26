d3d3f690cd8bb2ce742f3ed40c14f7f63ed7a997
Jiusen <2434732570@qq.com>
2021-01-26 17:52:55 +0800

    Stream消息驱动之生产者

---
+++ cloud-stream-rabbitmq-consumer8802/src/main/java/com/atguigu/springcloud/StreamMQMain8802.java
@@ -0,0 +1,17 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Jiusen Guo
+ * @date 2021/1/26 17:21
+ */
+@SpringBootApplication
+public class StreamMQMain8802 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StreamMQMain8802.class, args);
+    }
+
+}
