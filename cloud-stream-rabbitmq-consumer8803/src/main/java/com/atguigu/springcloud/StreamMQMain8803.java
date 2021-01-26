dc6bba817bcef51d64e3aa715efb0aea7b9f110e
Jiusen <2434732570@qq.com>
2021-01-26 18:33:09 +0800

    Stream之消息重复消费+解决

---
+++ cloud-stream-rabbitmq-consumer8803/src/main/java/com/atguigu/springcloud/StreamMQMain8803.java
@@ -0,0 +1,17 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Jiusen Guo
+ * @date 2021/1/26 18:01
+ */
+@SpringBootApplication
+public class StreamMQMain8803
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(StreamMQMain8803.class,args);
+    }
+}
